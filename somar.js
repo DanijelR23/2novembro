a79118825ed3f6f7c7108ee1d37d3e2d0b2289bf
DanijelR23 <danielrodrigues2326@gmail.com>
2023-11-02 11:36:02 +0000

    funcao somar

---
+++ somar.js
@@ -0,0 +1,7 @@
+function somarNumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+// Exemplo de utilização da função para somar 3 e 5
+const resultado = somarNumeros(3, 5);
+console.log("O resultado da soma é: " + resultado); // Saída: O resultado da soma é: 8
