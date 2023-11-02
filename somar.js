4304aa9d97207d5cd7186bd14de6ab46e8890fb7
DanijelR23 <danielrodrigues2326@gmail.com>
2023-11-02 11:36:52 +0000

    somar v2

--- somar.js
+++ somar.js
@@ -2,6 +2,3 @@ function somarNumeros(numero1, numero2) {
     return numero1 + numero2;
 }
 
-// Exemplo de utilização da função para somar 3 e 5
-const resultado = somarNumeros(3, 5);
-console.log("O resultado da soma é: " + resultado); // Saída: O resultado da soma é: 8
